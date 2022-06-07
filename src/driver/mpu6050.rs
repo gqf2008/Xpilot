8ba7514db3ae33eb5ccc92e298394f2b49e86b0c
高庆丰 <gao.qingfeng@gmail.com>
2022-06-07 15:33:18 +0800

    no message

--- src/driver/mpu6050.rs
+++ src/driver/mpu6050.rs
@@ -1,4 +1,4 @@
-use crate::driver::{Accel, Gyro, Quaternion};
+use crate::driver::{Accel, Gyro, ImuData};
 use core::fmt::Formatter;
 use embedded_hal::blocking::i2c::{Write, WriteRead};
 pub struct Mpu6050<I2c>
@@ -365,93 +365,7 @@ impl RawData {
                 y: self.gy as f32 / gyro_range,
                 z: self.gz as f32 / gyro_range,
             },
-        }
-    }
-}
-
-#[derive(Debug, Default)]
-pub struct ImuData {
-    pub accel: Accel,
-    pub temp: f32,
-    pub gyro: Gyro,
-}
-
-impl ImuData {
-    pub fn to_quat(self) -> Quaternion {
-        unsafe {
-            use libm::*;
-            #[allow(non_upper_case_globals)]
-            const Kp: f32 = 100.0; // 比例增益支配率收敛到加速度计/磁强计
-            #[allow(non_upper_case_globals)]
-            const Ki: f32 = 0.002; // 积分增益支配率的陀螺仪偏见的衔接
-
-            const HALF_T: f32 = 0.001; // 采样周期的一半
-            #[allow(non_upper_case_globals)]
-            static mut q0: f32 = 1.0; // 四元数的元素，代表估计方向
-            #[allow(non_upper_case_globals)]
-            static mut q1: f32 = 0.0;
-            #[allow(non_upper_case_globals)]
-            static mut q2: f32 = 0.0;
-            #[allow(non_upper_case_globals)]
-            static mut q3: f32 = 0.0;
-
-            // 按比例缩小积分误差
-            #[allow(non_upper_case_globals)]
-            static mut ex_int: f32 = 0.0;
-            #[allow(non_upper_case_globals)]
-            static mut ey_int: f32 = 0.0;
-            #[allow(non_upper_case_globals)]
-            static mut ez_int: f32 = 0.0;
-            let mut ax = self.accel.x;
-            let mut ay = self.accel.y;
-            let mut az = self.accel.z;
-            let mut gx = self.gyro.x;
-            let mut gy = self.gyro.y;
-            let mut gz = self.gyro.z;
-            // 测量正常化
-            let mut norm = sqrtf(ax * ax + ay * ay + az * az);
-            ax = ax / norm; //单位化
-            ay = ay / norm;
-            az = az / norm;
-
-            // 估计方向的重力
-            let vx = 2.0 * (q1 * q3 - q0 * q2);
-            let vy = 2.0 * (q0 * q1 + q2 * q3);
-            let vz = q0 * q0 - q1 * q1 - q2 * q2 + q3 * q3;
-
-            // 错误的领域和方向传感器测量参考方向之间的交叉乘积的总和
-            let ex = ay * vz - az * vy;
-            let ey = az * vx - ax * vz;
-            let ez = ax * vy - ay * vx;
-
-            // 积分误差比例积分增益
-            ex_int = ex_int + ex * Ki;
-            ey_int = ey_int + ey * Ki;
-            ez_int = ez_int + ez * Ki;
-
-            // 调整后的陀螺仪测量
-            gx = gx + Kp * ex + ex_int;
-            gy = gy + Kp * ey + ey_int;
-            gz = gz + Kp * ez + ez_int;
-
-            // 整合四元数率和正常化
-            q0 = q0 + (-q1 * gx - q2 * gy - q3 * gz) * HALF_T;
-            q1 = q1 + (q0 * gx + q2 * gz - q3 * gy) * HALF_T;
-            q2 = q2 + (q0 * gy - q1 * gz + q3 * gx) * HALF_T;
-            q3 = q3 + (q0 * gz + q1 * gy - q2 * gx) * HALF_T;
-
-            // 正常化四元
-            norm = sqrtf(q0 * q0 + q1 * q1 + q2 * q2 + q3 * q3);
-            q0 = q0 / norm;
-            q1 = q1 / norm;
-            q2 = q2 / norm;
-            q3 = q3 / norm;
-            Quaternion {
-                w: q0,
-                x: q1,
-                y: q2,
-                z: q3,
-            }
+            compass: Default::default(),
         }
     }
 }
